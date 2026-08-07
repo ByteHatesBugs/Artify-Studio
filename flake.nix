c0403f3ca46100a48ff9f39f80b48c1255f6a409
ByteHatesBugs <u2m0a1r3ktk@gmail.com>
2026-08-07 14:59:07 +0500

    feat(nix): add reproducible NixOS dev shell

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Artify Studio development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+              ffmpeg
+              git
+            ];
+
+            FFMPEG_PATH = "${pkgs.ffmpeg}/bin/ffmpeg";
+
+            shellHook = ''
+              echo "Artify Studio environment ready"
+              echo "Bun: $(bun --version) | FFmpeg: $(ffmpeg -version | head -n 1)"
+            '';
+          };
+        }
+      );
+    };
+}
